3b5b4ad23526145e3da7b16d3227aa66f56430bc
Lucas Pinheiro <131314634+lucaspinheiroo@users.noreply.github.com>
2023-09-13 23:24:10 -0400

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int idade = 32;
+        double saldo = 10.35784;
+        string nome = "Maria";
+        Console.WriteLine("{0} tem {1} anos e tem saldo de {2:F2} reais", nome, idade, saldo);
+        Console.WriteLine($"{nome} tem {idade} anos e tem saldo de {saldo:F2} reais");
+        Console.WriteLine(nome + " tem " + idade + " anos e tem saldo de " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais ");
+
+    }
+}
---
+++ ExercicioDeFixação/ExercicioDeFixação/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace exercicio
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine($"{produto1}, cujo preço é $ {preco1.ToString("F1", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{produto2}, cujo preço é $ {preco2.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            Console.WriteLine($"Registro:{idade} anos de idade, código {codigo} e gênero {genero} ");
+            Console.WriteLine($"Medida com oito casa decimais: {medida:F8}");
+            Console.WriteLine($"Arredondado: {medida:F3}");
+            Console.WriteLine($"Separador decimal invariante culture: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
